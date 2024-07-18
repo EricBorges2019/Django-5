0fec46578552991b426ff0a21e9c672e12e22d85
ericborges2019 <ericborges2019@fau.edu>
2024-07-18 11:49:34 -0400

    Homepage now renders from HTML!

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import django.template
+from django.template.loader import get_template
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Homepage index.")
+    print(dir_path)
+    get_template("index.html")
+    return render(request, "index.html")
